f402a68ea30355ad35f15f64b6d556074ca89a1e
MarcCDJ <marccruzdj@gmail.com>
2023-07-29 17:55:39 -0400

    Create model

---
+++ BB Quotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  BB Quotes
+//
+//  Created by Marc Cruz on 7/29/23.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+    let production: String
+}
