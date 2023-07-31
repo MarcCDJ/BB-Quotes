d5d3450f45d03a813f9e0471029f2171e1699f84
MarcCDJ <marccruzdj@gmail.com>
2023-07-31 11:58:32 -0400

    Add quote view

---
+++ BB Quotes/QuoteView.swift
@@ -0,0 +1,54 @@
+//
+//  QuoteView.swift
+//  BB Quotes
+//
+//  Created by Marc Cruz on 7/31/23.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                Image("breakingbad")
+                    .resizable()
+                    .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+                
+                VStack {
+                    Text("\"You either run from things, or youo face them, Mr. White\"")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.black.opacity(0.5))
+                        .cornerRadius(25)
+                        .padding(.horizontal)
+                    
+                    ZStack(alignment: .bottom) {
+                        Image("jessepinkman")
+                            .resizable()
+                            .scaledToFill()
+                        
+                        Text("Jesse Pinkman")
+                            .foregroundStyle(.white)
+                            .padding(10)
+                            .frame(maxWidth: .infinity)
+                            .background(.ultraThinMaterial)
+                    }
+                    .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+                    .cornerRadius(80)
+                }
+                .frame(width: geo.size.width)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct QuoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuoteView()
+            .preferredColorScheme(.dark)
+    }
+}
